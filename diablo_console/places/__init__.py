f45cb376c213661a2889303e963dee330127202d
drootnar <drootnar@gmail.com>
2016-11-21 17:40:01 +0100

    #25 add building places

--- diablo_console/places/__init__.py
+++ diablo_console/places/__init__.py
@@ -1,11 +1,28 @@
 from .terrain import *
+from .building import *
 
 places = {
-	'w': Lake,
-	'W': Ocean,
-	'.': Dirt,
-	'f': Forest,
-	'^': Mountain,
-	'!': MountainPeak,
-	'g': Grass
+    'w': Lake,
+    'W': Ocean,
+    '.': Dirt,
+    'f': Forest,
+    '^': Mountain,
+    '!': MountainPeak,
+    'g': Grass,
+    '/': RoofLeft,
+    '\\': RoofRight,
+    '┃': HouseVert,
+    '━': HouseHor,
+    '┻': HouseBottomMiddle,
+    '┳': HouseUpMiddle,
+    '┏': HouseLeftUp,
+    '┗': HouseLeftBottom,
+    '┣': HouseLeftMiddle,
+    '┓': HouseRightUp,
+    '┛': HouseRightBottom,
+    '┫': HouseRightMiddle,
+    '╋': HouseIntersection,
+    '░': HouseWall1,
+    '▒': HouseWall2,
+    '▓': HouseWall3
 }
---
+++ diablo_console/places/building.py
@@ -0,0 +1,88 @@
+from ..const import *
+
+__all__ = [
+    'RoofLeft', 'RoofRight', 'HouseVert', 'HouseHor', 'HouseBottomMiddle', 'HouseUpMiddle',
+    'HouseLeftUp', 'HouseLeftBottom', 'HouseLeftMiddle', 'HouseRightUp', 'HouseRightBottom',
+    'HouseRightMiddle', 'HouseIntersection', 'HouseWall1', 'HouseWall2', 'HouseWall3']
+
+class Building:
+    type = 'building'
+
+
+class RoofLeft(Building):
+    name = 'roof'
+    key = '/'
+    description = 'That is a roof'
+    efect = C_MAGENTA
+    enterable = False
+
+
+class RoofRight(Building):
+    name = 'roof'
+    key = '\\'
+    description = 'That is a roof'
+    efect = C_MAGENTA
+    enterable = False
+
+
+class House(Building):
+    name = 'house'
+    description = 'That is a house'
+    efect = C_MAGENTA
+    enterable = False
+
+
+class HouseVert(House):
+    key = '┃'
+    
+
+class HouseHor(House):
+    key = '━'
+
+
+class HouseBottomMiddle(House):
+    key = '┻'
+
+
+class HouseUpMiddle(House):
+    key = '┳'
+
+
+class HouseLeftUp(House):
+    key = '┏'
+
+
+class HouseLeftBottom(House):
+    key = '┗'
+
+
+class HouseLeftMiddle(House):
+    key = '┣'
+
+
+class HouseRightUp(House):
+    key = '┓'
+
+
+class HouseRightBottom(House):
+    key = '┛'
+
+
+class HouseRightMiddle(House):
+    key = '┫'
+
+
+class HouseIntersection(House):
+    key = '╋'
+
+
+class HouseWall1(House):
+    key = '░'
+
+
+class HouseWall2(House):
+    key = '▒'
+
+
+class HouseWall3(House):
+    key = '▓'
